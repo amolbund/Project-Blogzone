c5044ab22161c06dbfc174d5c2c7d7d24d6186ce
amolbund <amolbund@gmail.com>
2022-04-13 10:26:35 +0530

    registration page created

---
+++ JS/login.js
@@ -0,0 +1,22 @@
+
+
+
+// $.ajax({ 
+//     type: "GET",
+//     dataType: "jsonp",
+//     url: "http://localhost:3000/users",
+//     success: function(data){        
+//       alert(data);
+//     }
+//  });
+
+// function getLoginData(email) {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             document.getElementById("response").innerHTML = this.responseText;
+//         }
+//     };
+//     xhttp.open("GET", "http://localhost:3000/user/"+email, true);
+//     xhttp.send();
+// }
